0c80010e6a6c2b3794a1b833b7f3f614891ca0e7
Jerome-del-ux <losperjerome00@yahoo.com>
2026-09-24 02:01:37 +0200

    Make Android app standalone with bundled UI

--- android/app/src/main/java/ai/jetboomer/app/MainActivity.kt
+++ android/app/src/main/java/ai/jetboomer/app/MainActivity.kt
@@ -1,8 +1,18 @@
 package ai.jetboomer.app
+
 import android.app.Activity
 import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
-class MainActivity: Activity() {
- override fun onCreate(savedInstanceState: Bundle?) { super.onCreate(savedInstanceState); val w=WebView(this); w.webViewClient=WebViewClient(); w.settings.javaScriptEnabled=true; w.loadUrl("https://jerome-del-ux.github.io/Trading-bot/"); setContentView(w) }
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val webView = WebView(this)
+        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.loadUrl("file:///android_asset/index.html")
+        setContentView(webView)
+    }
 }
